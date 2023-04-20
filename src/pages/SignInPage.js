f2bab805c66315bc1f785539554087563431acde
Thiana <thianacantunes@gmail.com>
2023-04-20 11:11:31 -0300

    versão com o requisito login conectado ao back

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,26 +1,44 @@
 import styled from "styled-components"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate} from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import axios from "axios"
+
 
 export default function SignInPage() {
+
+  const [form, setForm] = useState({email:"", senha:""})
   const navigate = useNavigate()
 
   function entrarNoApp (e){
     e.preventDefault()
-    navigate("/home")
+
+    //eslint-disable-next-line no-undef
+    axios.post(`${process.env.REACT_APP_API}/login`, form)
+    .then((res) => navigate("/home"))
+    .catch((err) => console.log(err))
+ // navigate("/home")
 
   }
-
-
 
 
   return (
     <SingInContainer>
       <form onSubmit={entrarNoApp}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input 
+        placeholder="E-mail" 
+        type="email" 
+        value={form.email}
+        onChange={(e) => setForm({...form, email: e.target.value})}
+        />
+        <input 
+        placeholder="Senha" 
+        type="password" 
+        autocomplete="new-password" 
+        value={form.senha}
+        onChange={(e) => setForm({...form, senha: e.target.value})}/>
+        <button type="submit">Entrar</button>
       </form>
 
       <Link to="/cadastro">
